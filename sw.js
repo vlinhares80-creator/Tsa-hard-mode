2d235111067dbf2f7c26b45029b9de2e61d2818e
vlinhares80-creator <vlinhares80@gmail.com>
2026-06-30 14:44:06 -0300

    Refactor service worker caching strategy and events

--- sw.js
+++ sw.js
@@ -1,8 +1,59 @@
-const CACHE = "tsa-hard-mode-v2-101q";
-const FILES = ["./", "index.html", "style.css", "app.js", "manifest.json", "icon.svg"];
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+const CACHE_NAME = "tsa-hard-mode-cache-v250";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html?v=250",
+  "./style.css?v=250",
+  "./questions.js?v=250",
+  "./app.js?v=250",
+  "./manifest.json?v=250",
+  "./icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE).catch(() => {
+        return Promise.resolve();
+      });
+    })
+  );
 });
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
 });
